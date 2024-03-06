57bc20f1f3a5bc100f446983778328bdd1c88f0d
T-Prakash2002 <prakasht1405@gmail.com>
2024-03-06 18:22:18 +0530

    modify

--- notes/script.js
+++ notes/script.js
@@ -1,13 +1,26 @@
-let promise=new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        reject('Resolved')
-    },5000)
-})
 
-promise
-    .then((message)=>{
-        console.log(message);
-    })
-    .catch((message)=>{
-        console.log(message+" error")
-    })
+const APIKey='e0e0b8854e4bee10ed0324f89bb0e134';
+
+function fetchWeather(city){
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`)
+        .then(response => response.json())
+        .then(weather=>{
+
+            console.log(Math.floor(weather.main.temp- 273.15));
+
+        })
+
+}
+
+
+
+function getWeather(e){
+
+    e.preventDefault();
+
+    let city=e.target.elements.city.value;
+
+    fetchWeather(city);
+}
+
